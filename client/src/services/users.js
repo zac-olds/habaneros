09a3d6f89f1b16a12ca16eba1f4ece1b4ef9a083
Zac Olds <zac.olds@gmail.com>
2021-01-06 13:17:56 -0600

    fixed sign in bug

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -3,22 +3,22 @@ import jwtDecode from 'jwt-decode'
 
 export const signUp = async credentials => {
     try {
-      const resp = await api.post('/sign-up', credentials)
-    localStorage.setItem('token', resp.data.token)
-          const user = jwtDecode(resp.data.token)
-          return user
-      } catch (error) {
-          throw error
-      }
+        const resp = await api.post('/sign-up', credentials)
+        localStorage.setItem('token', resp.data.token)
+        const user = jwtDecode(resp.data.token)
+        return user
+    } catch (error) {
+        throw error
+    }
   }
 
 
 export const signIn = async credentials => {
     try {
       const resp = await api.post('/sign-in', credentials)
-    localStorage.setItem('token', resp.data.token)
-    const user = jwtDecode(resp.data.token)
-    return user
+      localStorage.setItem('token', resp.data.token)
+      const user = jwtDecode(resp.data.token)
+      return user
   } catch (error) {
       throw error
   }
--- helpers/restrict.js
+++ helpers/restrict.js
@@ -5,7 +5,8 @@ const TOKEN_KEY = 'areallylonggoodkey'
 
         try {
           const token = req.headers.authorization.split(' ')[1]
-          const data = jwt.verify(token, TOKEN_KEY) 
+          const data = jwt.verify(token, TOKEN_KEY)
+          // res.locals.user = data;
         next()
       } catch (error) {
           console.log(error)
