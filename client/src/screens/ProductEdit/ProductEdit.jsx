05d23664acaf67190260ddf54a5bf917c5440880
Jeff Bond <jeffreybond@mac.com>
2021-01-06 10:10:45 -0500

    first pass on ProductEdit.jsx

---
+++ client/src/screens/ProductEdit/ProductEdit.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import './ProductEdit.css'
+import { useParams, Redirect } from 'react-router-dom'
+import Layout from '../../components/shared/Layout/Layout'
+import { getProduct, updateProduct } from '../../services/products'
+import ProductDetail from '../ProductDetail/ProductDetail'
+ 
+
+const ProductEdit = (props) => {
+
+
+}
+
+export default ProductEdit
