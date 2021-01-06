c7743c57dabf8150b1f28c2d45b8856e6175520d
Jeff Bond <jeffreybond@mac.com>
2021-01-06 10:01:05 -0500

    basic skeleton for ProductDetail.jsx

---
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import './ProductDetail.css'
+import Layout from '../../components/shared/Layout/Layout'
+import { getProduct, deleteProduct } from '../../services/products'
+import { useParams, Link } from 'react-router-dom'
+
+const ProductDetail = (props) => {
+
+
+}
+
+export default ProductDetail
