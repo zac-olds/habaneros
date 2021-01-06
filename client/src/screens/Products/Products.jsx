1897a0e41d9f3d1cb7a6eebb8eafaac26f353dfa
Jeff Bond <jeffreybond@mac.com>
2021-01-06 10:23:10 -0500

    first commit for  Products

---
+++ client/src/screens/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import './Products.css'
+import Product from '../../components/Product/Product'
+import Search from '../../components/Search/Search'
+import { AZ, ZA, lowestFirst, highestFirst } from "../../utils/sort"
+import Sort from '../../components/Sort/Sort'
+import Layout from '../../components/shared/Layout/Layout'
+import { getProducts } from '../../services/products'
+
+const Products = {
+
+
+}
+
+expost default Products
