575f3ad5cd49ecb62f18034316c47650cae7e517
Zac Olds <zac.olds@gmail.com>
2021-01-11 20:41:32 -0600

    clearing react errors

--- client/src/components/PopupScoville/Popup.jsx
+++ client/src/components/PopupScoville/Popup.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './Popup.css';
+import React from "react";
+import "./Popup.css";
 
-function Popup (props) {
-  
-    return (
-      <div className='popup'>
-        <div className='popup_inner'>
-          {/* <p type="text" className='popup-p'>
+function Popup(props) {
+  return (
+    <div className="popup">
+      <div className="popup_inner">
+        {/* <p type="text" className='popup-p'>
             {props.text}</p> */}
-          <img className='popup-img'src = {props.img}></img>
-        <button className='popup-btn' onClick={props.closePopup}>Close Me</button>
-        </div>
+        <img className="popup-img" src={props.img} alt="scoville-chart"></img>
+        <button className="popup-btn" onClick={props.closePopup}>
+          Close Me
+        </button>
       </div>
-    );
- 
+    </div>
+  );
 }
 
 export default Popup;
-
