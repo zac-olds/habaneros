4fca0ae572ceb548dca48e1ca6e648717c1e9b9a
Zac Olds <zac.olds@gmail.com>
2021-01-12 08:37:28 -0600

    syntax

--- client/src/components/PopupScoville/Popup.jsx
+++ client/src/components/PopupScoville/Popup.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Popup.css";
 
-function Popup (props) {
-  
-    return (
-      <div>
-        <div className='popup_inner'>
-          <div className='popup-img' src={props.img}></div>
-          <button className='popup-btn' onClick={props.closePopup}>Close Me</button>
-        </div>
+function Popup(props) {
+  return (
+    <div>
+      <div className="popup_inner">
+        <div className="popup-img" src={props.img}></div>
+        <button className="popup-btn" onClick={props.closePopup}>
+          Close Me
+        </button>
       </div>
     </div>
   );
